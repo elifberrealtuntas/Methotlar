5c9a810bde5dea5a60335331a68fb3fb050eae36
elif berre altuntaş <elifberrealtuntas@gmail.com>
2021-01-17 00:20:06 +0300

    Ders Sonu 16 Ocak

---
+++ Methotlar/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Methotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            double fiyati = 15;
+            string aciklama = "Amasya Elması";
+
+            string[] meyveler = new string[] { };
+
+
+
+
+           Urun urun1= new Urun();
+           urun1.Adi = "Elma";
+           urun1.Fiyati = 15;
+           urun1.Aciklama = "Amasya Elması";
+
+
+           Urun urun2 = new Urun();
+           urun2.Adi = "Karpuz";
+           urun2.Fiyati = 80;
+           urun2.Aciklama = "Diyarbakır Karpuzu";
+
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+
+            foreach (Urun urun in urunler)
+
+            {
+                Console.WriteLine();
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("-----------------");
+
+
+            }
+
+            Console.WriteLine("-------------Methotlar---------------------");
+
+            //Instance - örnek class örneği çağırma
+            //encapsulation
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yeşil armut", 12,10);
+            sepetManager.Ekle2("Elma", "Yeşil elma", 12,9);
+            sepetManager.Ekle2("Kapuz", "Diyarbakır karpuzu", 12,8);
+
+            
+
+
+
+        }
+    }
+}
+//Dont repeat yourself - DRY - Clean Code - Best Practice
